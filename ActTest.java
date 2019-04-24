311c9b9a0b55e9789566d7e8b88f968cbcbd0c22
Igor <carvalho.igor@live.com>
2019-04-24 00:52:48 -0300

    Update ActTest.java

--- ActTest.java
+++ ActTest.java
@@ -13,10 +13,10 @@ public class ActTest
 	public static void main(String[] args)
 	{
 		//instance of object scanner
-    	Scanner ler = new Scanner(System.in);
-    	//instance of string and read keybord until press enter key
-    	String orderString = ler.nextLine();
-		//instance
+		Scanner ler = new Scanner(System.in);
+		//instance of string and read keybord until press enter key
+		String orderString = ler.nextLine();
+		//instance of object order
 		Order order = new Order(orderString); 	
   		//order exibition	
 		order.show();	
